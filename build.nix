3188b793c5a98a33ca8fe234be44f4b3cee29760
Jason Lieb <Jason.lieb@outlook.com>
2024-12-01 19:10:29 -0500

    init

---
+++ build.nix
@@ -0,0 +1,26 @@
+{
+  pkgs ? (
+    let
+      inherit (builtins) fetchTree fromJSON readFile;
+      inherit ((fromJSON (readFile ./flake.lock)).nodes) nixpkgs gomod2nix;
+    in
+    import (fetchTree nixpkgs.locked) {
+      overlays = [ (import "${fetchTree gomod2nix.locked}/overlay.nix") ];
+    }
+  ),
+  buildGoApplication ? pkgs.buildGoApplication,
+  script,
+}:
+
+let
+  scripts = {
+    hello = buildGoApplication {
+      pname = "day1";
+      version = "0.1";
+      go = pkgs.go_1_23;
+      src = ./scripts/day1;
+      modules = ./scripts/day1/gomod2nix.toml;
+    };
+  };
+in
+scripts.${script}
---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Advent of Code 2024";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.gomod2nix.url = "github:nix-community/gomod2nix";
+  inputs.gomod2nix.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.gomod2nix.inputs.flake-utils.follows = "flake-utils";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      gomod2nix,
+    }:
+    (flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        callPackage = pkgs.callPackage;
+        buildPackage =
+          script:
+          callPackage ./build.nix {
+            inherit (gomod2nix.legacyPackages.${system}) buildGoApplication;
+            inherit script;
+          };
+
+        packages = {
+          day1 = buildPackage "day1";
+        };
+
+        packageOutputs = builtins.attrValues packages;
+      in
+      {
+        packages = packages // {
+          default = pkgs.runCommand "default" { buildInputs = packageOutputs; } ''
+            mkdir -p $out/bin
+            for pkg in ${builtins.concatStringsSep " " (map (pkg: "${pkg}/bin/*") packageOutputs)}; do
+              cp $pkg $out/bin
+            done
+          '';
+        };
+
+        devShells.default = callPackage ./shell.nix {
+          inherit (gomod2nix.legacyPackages.${system}) mkGoEnv gomod2nix;
+        };
+      }
+    ));
+}
